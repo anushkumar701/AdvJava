21b0d7ba181dae91692f50efc88c8b8d7157d5b8
anushkumar701 <anushk0908@gmail.com>
2026-07-23 19:58:53 +0530

    Login Page

--- Servlets/servlet/src/main/java/com/CreateServlet.java
+++ Servlets/servlet/src/main/java/com/CreateServlet.java
@@ -11,8 +11,19 @@ import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/create")
 public class CreateServlet extends GenericServlet{
+
 	@Override
-	public void service(ServletRequest req,ServletResponse res) throws IOException,ServletException{
-	
+	public void service(ServletRequest req,ServletResponse res)throws IOException, ServletException{
+		String name= req.getParameter("name");
+		String email= req.getParameter("email");
+		String password= req.getParameter("password");
+		String phone= req.getParameter("phone");
+		
+		PrintWriter out=res.getWriter();
+		
+		out.print("<h1> Name = "+name+"</h1>");
+		out.print("<h1> Email = "+email+"</h1>");
+		out.print("<h1> Password = "+password+"</h1>");
+		out.print("<h1> Phone = "+phone+"</h1>");
 	}
 }
